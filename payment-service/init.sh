92b481fc32dd97592ea791e49ab4d4b7c271dc21
mateusf93 <mateusferreirasilva@hotmail.com>
2024-02-27 21:45:11 -0300

    Algumas melhroas de execução

--- payment-service/init.sh
+++ payment-service/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Execute Prisma migrations
+
 sleep 45 && npx prisma migrate dev --name init
 
-# Generate Prisma client
+
 sleep 2 && npx prisma generate
 
-# Build the application
+
 sleep 2 && npm run build
 
-# Start the application
+
 sleep 2 && npm run start
